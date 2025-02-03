4892ba7c5e16f8b2dce3c341c37c6f731471e80d
Robert Drucker <robert.drucker@holycitysoftware.com>
2025-02-03 08:15:13 -0500

    Lecture 43 - Instance Variables & Configurable Widgets

--- 2_roll_dice_app/lib/styled_text.dart
+++ 2_roll_dice_app/lib/styled_text.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
 class StyledText extends StatelessWidget {
-  const StyledText({super.key});
+  const StyledText(this.text, {super.key});
+
+  // instance variable
+  final String text;
 
   @override
   Widget build(context) {
-    return const Text(
-      'Hello World!',
-      style: TextStyle(
+    return Text(
+      text,
+      style: const TextStyle(
         color: Colors.white,
         fontSize: 28,
       ),
